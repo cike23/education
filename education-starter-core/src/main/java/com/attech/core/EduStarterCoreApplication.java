7526a3a92339d51bda930a72593358f77f90b8bd
Aaron <1057901123@qq.com>
2018-07-10 20:56:58 +0800

    first commit

---
+++ education-starter-core/src/main/java/com/attech/core/EduStarterCoreApplication.java
@@ -0,0 +1,20 @@
+package com.attech.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 项目启动入口
+ *
+ * @author Aaron
+ * @version v1.0
+ * @create 2018-07-10 20:28
+ **/
+@SpringBootApplication
+public class EduStarterCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduStarterCoreApplication.class, args);
+    }
+
+}
